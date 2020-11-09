0a55895cae29e506c8166ed7dd508c31dc5b0547
David Mecalco Alejo <dmecalcoa1200@alumno.ipn.mx>
2020-11-09 10:12:40 -0600

    Añadimos la nueva carpera de Enumeración

---
+++ Enumeración Exhaustiva/enumeracion.py
@@ -0,0 +1,11 @@
+objetivo = int(input('Escoje un entero: '))
+respuesta = 0
+
+while respuesta**2 < objetivo:
+    print(respuesta)
+    respuesta += 1
+
+if respuesta**2 == objetivo:
+    print(f'La Raiz cuadra de {objetivo} es {respuesta}')
+else:
+    print(f'{objetivo} no tiene una raiz cuadrada exacta')
